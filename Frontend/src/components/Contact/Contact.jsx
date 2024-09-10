63fe151fafcf9db601e46fd4a7f8587504dc7f83
Clovis <cpatoue@gmail.com>
2024-09-10 10:42:00 +0200

    ajout du captcha pour l'envoie du formulaire

--- Frontend/src/components/Contact/Contact.jsx
+++ Frontend/src/components/Contact/Contact.jsx
@@ -1,36 +1,49 @@
-import React, { useState } from 'react';
-import './Contact.scss';
+import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import "./Contact.scss";
+
 const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [status, setStatus] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState("");
+  const [captchaToken, setCaptchaToken] = useState("");
+
+  const handleCaptchaChange = (value) => {
+    setCaptchaToken(value);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const contactData = { name, email, message };
+    if (!captchaToken) {
+      setStatus("Veuillez vérifier le CAPTCHA.");
+      return;
+    }
+
+    const contactData = { name, email, message, captchaToken };
 
     try {
-      const response = await fetch('http://localhost:4000/api/contact', {
-        method: 'POST',
+      const response = await fetch("http://localhost:4000/api/contact", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(contactData),
       });
 
       if (response.ok) {
-        setStatus('Message envoyé avec succès!');
-        setName('');
-        setEmail('');
-        setMessage('');
+        setStatus("Message envoyé avec succès!");
+        setName("");
+        setEmail("");
+        setMessage("");
+        setCaptchaToken("");
       } else {
-        setStatus('Erreur lors de l\'envoi du message.');
+        setStatus("Erreur lors de l'envoi du message.");
       }
     } catch (error) {
-      console.error('Erreur:', error);
-      setStatus('Erreur lors de l\'envoi du message.');
+      console.error("Erreur:", error);
+      setStatus("Erreur lors de l'envoi du message.");
     }
   };
 
@@ -64,6 +77,11 @@ const ContactForm = () => {
             required
           ></textarea>
         </div>
+        <ReCAPTCHA
+          sitekey="6Lf-tjwqAAAAAK3OpWb1tUIPGr0CqVOfrOXSqEC0"
+          onChange={handleCaptchaChange}
+          className="captcha"
+        />
         <button type="submit">Envoyer</button>
       </form>
       {status && <p>{status}</p>}
