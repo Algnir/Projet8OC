42f150130a039b9f5bfcb2eb843ad1ccb0373bb2
Clovis <cpatoue@gmail.com>
2024-09-10 12:17:53 +0200

    ajout du component Banner

---
+++ Frontend/src/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import './Banner.scss';
+
+function Banner() {
+  return (
+    <>
+    <section id="accueil">
+        <div className="banner">
+            <h2>
+                Bienvenue sur mon Portfolio de développeur web
+            </h2>
+        </div>
+    </section>
+    </>
+  );
+}
+
+export default Banner;
