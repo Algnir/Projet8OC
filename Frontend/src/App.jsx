7c6fc0447bebb4a9ac74145e6b2c72b46529e26a
Clovis <cpatoue@gmail.com>
2024-09-10 12:17:02 +0200

    déplacement du scss du header et ajout du futur component banner dans app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import './App.scss';
 import Header from './components/Header/Header.jsx';
+import Banner from './components/Banner/Banner.jsx';
 import Presentation from './components/Presentation/Presentation.jsx';
-import Competence from './components/Competence/Competence.jsx';
+import Competences from './components/Competence/Competence.jsx';
 import Contact from './components/Contact/Contact.jsx';
 
 function App() {
 
   return (
     <>
-    <Header/>
     <main>
+      <Banner/>
+      <Header/>
       <Presentation/>
-      <Competence/>
+      <Competences/>
       <Contact/>
     </main>
     </>
