4793785715f3b6ed8fd9ad1df30a7b6028599251
Clovis <cpatoue@gmail.com>
2024-09-11 09:56:35 +0200

    arrangement de design css/image

--- Frontend/src/components/Banner/Banner.jsx
+++ Frontend/src/components/Banner/Banner.jsx
@@ -1,15 +1,29 @@
-import './Banner.scss';
+import "./Banner.scss";
 
 function Banner() {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <>
-    <section id="accueil">
+      <section id="accueil">
         <div className="banner">
-            <h2>
-                Bienvenue sur mon Portfolio de développeur web
-            </h2>
+          <h2>Bienvenue sur mon Portfolio de développeur web</h2>
         </div>
-    </section>
+        <div
+          className="scroll"
+          onClick={(e) => {
+            e.preventDefault();
+            scrollToSection("presentation");
+          }}
+        >
+          <p>⮟</p>
+        </div>
+      </section>
     </>
   );
 }
