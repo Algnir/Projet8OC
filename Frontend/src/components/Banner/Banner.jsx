bb9e0a5e5af1145de2f9148a857fa97baa6c56d2
Clovis <cpatoue@gmail.com>
2024-09-16 15:25:44 +0200

    ajout des particles sur la banner

--- Frontend/src/components/Banner/Banner.jsx
+++ Frontend/src/components/Banner/Banner.jsx
@@ -1,4 +1,5 @@
 import "./Banner.scss";
+import ParticlesComponent from './particles.jsx';
 
 function Banner() {
   const scrollToSection = (id) => {
@@ -9,22 +10,23 @@ function Banner() {
   };
 
   return (
-    <>
-      <section id="accueil">
-        <div className="banner">
-          <h2>Bienvenue sur mon Portfolio de développeur web</h2>
-        </div>
-        <div
-          className="scroll"
-          onClick={(e) => {
-            e.preventDefault();
-            scrollToSection("presentation");
-          }}
-        >
-          <p>⮟</p>
-        </div>
-      </section>
-    </>
+    <section id="accueil">
+      <div className="particles-container">
+      <ParticlesComponent id="particles" />
+      </div>
+      <div className="banner">
+        <h2>Bienvenue sur mon Portfolio de développeur web</h2>
+      </div>
+      <div
+        className="scroll"
+        onClick={(e) => {
+          e.preventDefault();
+          scrollToSection("presentation");
+        }}
+      >
+        <p>⮟</p>
+      </div>
+    </section>
   );
 }
 
