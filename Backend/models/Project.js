84ad08f63a54a48410a9cb0d2869ac2dc3c6ff28
Clovis <cpatoue@gmail.com>
2024-09-16 15:24:56 +0200

    changement du model pour la modal

--- Backend/models/Project.js
+++ Backend/models/Project.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const projectSchema = mongoose.Schema({
-    id: { type: String, required: true },
-    title: { type: String, required: true },
-    img: { type: String, required: true },
-    description: { type: String },
-    github: { type: String },
-    tag: { type: String, required: true },
-    logo: { type: [String], required: true },
-  });
+  id: { type: String, required: true },
+  title: { type: String, required: true },
+  img: { type: String, required: true },
+  description: { type: String },
+  github: { type: String },
+  tag: { type: String, required: true },
+  logo: { type: [String], required: true },
+  modal: {
+    context: { type: String, required: true },
+    learned: { type: String, required: true },
+  },
+});
 
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model("Project", projectSchema);
