8117f21935b148b785b5db2ec7c7fdf665a580b8
Clovis <cpatoue@gmail.com>
2024-09-22 12:13:42 +0200

    ajout des alt pour les logo et correction pour les images

--- Frontend/src/components/Projets/Card.jsx
+++ Frontend/src/components/Projets/Card.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import githubsvg from "../../images/github.svg";
 import Modal from "react-modal";
+
 const Card = ({
   image,
   title,
@@ -9,13 +10,14 @@ const Card = ({
   logo,
   context,
   learned,
-  alt
+  alt,
 }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const gitOpen = () => {
     window.open(githubLink, "_blank");
   };
+
   const openModal = () => {
     setModalIsOpen(true);
   };
@@ -27,19 +29,28 @@ const Card = ({
   return (
     <>
       <div className="card" onClick={openModal}>
-        <img src={image} className="card-image" />
+        <img src={image} className="card-image" alt={alt} />
         <h3 className="card-title">{title}</h3>
         <p className="card-description">{description}</p>
         <button className="git-button" onClick={gitOpen}>
-          <img src={githubsvg} />
+          <img src={githubsvg} alt="github icon" />
           Github
         </button>
         <div className="logo-container">
-          {logo.map((item, index) => (
-            <img key={index} src={item} className="logo" />
-          ))}
+          {logo.map((item, index) => {
+            const logoName = item.split("/").pop().split(".")[0]; 
+            return (
+              <img
+                key={index}
+                src={item}
+                className="logo-modal"
+                alt={`logo de ${logoName}`}
+              />
+            );
+          })}
         </div>
       </div>
+
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -47,20 +58,28 @@ const Card = ({
         className="modal-content"
         overlayClassName="modal-overlay"
       >
-        <h3 className="">{title}</h3>
+        <h3>{title}</h3>
         <h4>Contexte du projet</h4>
         <p>{context}</p>
         <h4>Ce que j'ai appris</h4>
         <p>{learned}</p>
         <h4>Technologies utilisées</h4>
         <div className="logo-container">
-          {logo.map((item, index) => (
-            <img key={index} src={item} className="logo-modal" alt={alt} />
-          ))}
+          {logo.map((item, index) => {
+            const logoName = item.split("/").pop().split(".")[0];
+            return (
+              <img
+                key={index}
+                src={item}
+                className="logo-modal"
+                alt={`logo de ${logoName}`}
+              />
+            );
+          })}
         </div>
         <div className="button-container">
           <button className="git-button" onClick={gitOpen}>
-            <img src={githubsvg} />
+            <img src={githubsvg} alt="github icon" />
             Github
           </button>
         </div>
