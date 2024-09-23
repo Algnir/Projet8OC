08acc6dc4e69a06a832d45c6a276857400005808
Clovis <cpatoue@gmail.com>
2024-09-23 10:08:01 +0200

    changement du model pour le alt de l'image principal

--- Backend/models/Project.js
+++ Backend/models/Project.js
@@ -4,6 +4,7 @@ const projectSchema = mongoose.Schema({
   id: { type: String, required: true },
   title: { type: String, required: true },
   img: { type: String, required: true },
+  alt: { type: String, required: true },
   description: { type: String },
   github: { type: String },
   tag: { type: String, required: true },
