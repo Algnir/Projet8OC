7137fdbf380dd9958da204a6806f741f16081e3e
Clovis <cpatoue@gmail.com>
2024-09-23 10:07:27 +0200

    ajout de path oublié pour la gestion d'image

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors'); 
 const bodyParser = require('body-parser');
+const path = require('path');
 const helmet = require('helmet'); 
 
 const projectsRoutes = require('./routes/projects');
